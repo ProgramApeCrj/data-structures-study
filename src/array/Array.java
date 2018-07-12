888290507da6b5c7fd8183d32219e068834163a8
programapecrj <java123456@foxmail>
2018-07-12 23:38:14 +0800

    设置基本的属性和办法

--- src/array/Array.java
+++ src/array/Array.java
@@ -3,11 +3,11 @@ package array;
 public class Array {
     
     private int[] data; // 存放的数组
-    private int size; // 当前容量
+    private int size; // 当前数组以存放的元素个数
     
     /**
      * 用户能确定大概容量时的构造函数
-     * 传入数组的容量capacity来构造Array
+     * 传入数组的最大容量capacity来构造Array
      * @param capacity
      */
     public Array(int capacity) {
@@ -23,5 +23,30 @@ public class Array {
         this(10); 
     }
     
+    /**
+     * 获取数组中元素个数
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
     
+    /**
+     * 获取数组的最大容量值
+     * @return
+     */
+    public int getCapacity() {
+        return data.length;
+    }
+    
+    /**
+     * 该数组是否为空
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+   
 }
+
+
